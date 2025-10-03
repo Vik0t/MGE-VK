159cb2c1e37da6b5ae376e5ac3d32b84bba467ce
ddimk <ddimka007@gmail.com>
2025-10-03 13:00:45 +0700

    basic page

---
+++ app/src/main/java/com/example/mge_vk/App.kt
@@ -0,0 +1,50 @@
+
+package com.example.mge_vk
+
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun App(
+    name: String,
+    icon: ImageVector,
+    tag: String,
+){
+    Row (
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ){
+        Row(
+
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = name,
+                modifier = Modifier.size(64.dp)
+            )
+            Column {
+                Text(name, color = MaterialTheme.colorScheme.primary)
+                Text("asd")
+            }
+
+        }
+        Button(onClick = {}) {
+            Text("Install")
+        }
+    }
+}
--- app/src/main/java/com/example/mge_vk/MainActivity.kt
+++ app/src/main/java/com/example/mge_vk/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mge_vk.ui.theme.MGEVKTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +37,79 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MGEVKTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
-}
+
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Screen(){
+    Column(Modifier.fillMaxHeight()
+        .padding(bottom = 16.dp, start = 16.dp, end = 16.dp, top = 16.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+
+    ) {
+        Column {
+            Title()
+            Spacer(Modifier.height(16.dp))
+            App(
+                name = "Приложение",
+                icon = Icons.Default.AccountBox,
+                tag = "month of games"
+            )
+            App(
+                name = "MAx",
+                icon = Icons.Default.AccountBox,
+                tag = "month of games"
+            )
+            App(
+                name = "VKontakte",
+                icon = Icons.Default.AccountBox,
+                tag = "month of games"
+            )
+
+
+        }
+        Footer()
+    }
+}
+@Composable
+fun Footer(){
+
+    Row(verticalAlignment = Alignment.Bottom,
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(imageVector = Icons.Default.AccountCircle,
+            modifier = Modifier.clickable(enabled = true, onClick = {})
+                .size(50.dp),
+            contentDescription = "lol"
+        )
+        Icon(imageVector = Icons.Default.Search,
+            modifier = Modifier.clickable(enabled = true, onClick = {})
+                .size(50.dp),
+            contentDescription = "lol"
+        )
+        Icon(imageVector = Icons.Default.AddCircle,
+            modifier = Modifier.clickable(enabled = true, onClick = {})
+                .size(50.dp),
+            contentDescription = "lol"
+        )
+        Icon(imageVector = Icons.Default.Favorite,
+            modifier = Modifier.clickable(enabled = true, onClick = {})
+                .size(50.dp),
+            contentDescription = "lol"
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Greeting(){
     MGEVKTheme {
-        Greeting("Android")
+        com.example.mge_vk.Screen()
     }
 }
